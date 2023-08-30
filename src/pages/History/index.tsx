1eb5a7483b495267f45f98566e44db31637b9dde
danielvalmeida91 <danielvalmeida91@gmail.com>
2023-08-30 16:41:04 -0300

    feat: Estilizando pagina History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,60 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <Status statusColor={'green'}>Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <Status statusColor={'green'}>Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <Status statusColor={'green'}>Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <Status statusColor={'green'}>Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <Status statusColor={'yellow'}>Em andamento</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 20 minutos</td>
+              <Status statusColor={'red'}>Interrompido</Status>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
